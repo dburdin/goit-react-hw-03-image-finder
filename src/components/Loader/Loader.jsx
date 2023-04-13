1dcf6910f85650918c9709413b3c4a69c55d96c4
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-04-13 13:15:13 +0300

    hw-03-img-finder-little-corrections-of-Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,19 +1,16 @@
 import { TailSpin } from 'react-loader-spinner';
-import { LoaderContainer } from './Loader.styled';
 
 export const Loader = () => {
   return (
-    <LoaderContainer>
-      <TailSpin
-        height="80"
-        width="80"
-        color="#3f51b5"
-        ariaLabel="tail-spin-loading"
-        radius="1"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
-    </LoaderContainer>
+    <TailSpin
+      height="80"
+      width="80"
+      color="#3f51b5"
+      ariaLabel="tail-spin-loading"
+      radius="1"
+      wrapperStyle={{ justifyContent: 'center' }}
+      wrapperClass=""
+      visible={true}
+    />
   );
 };
--- src/components/Loader/Loader.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const LoaderContainer = styled.div`
-  width: auto;
-  display: flex;
-  justify-content: center;
-  margin-top: 50px;
-`;
